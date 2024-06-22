c2eef9e4e04ebd2bcc2065b6c35381231caffb14
Alleyway-boop <3181138299@qq.com>
2024-06-22 21:15:15 +0800

    remove sign in console

--- script/CallReplaceScript.cjs
+++ script/CallReplaceScript.cjs
@@ -11,7 +11,7 @@ fs.access(exePath, fs.constants.F_OK, (err) => {
   const childProcess = require('child_process');
   const child = childProcess.spawn(exePath, ['-d', 'dist', '-s', '__uno', '-r', 'uno', '-c', '-n']);
   child.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
+    console.log(`${data}`);
     fs.appendFileSync(path.join(callerPath, 'output.log'), data);
   });
   child.stderr.on('data', (data) => {
