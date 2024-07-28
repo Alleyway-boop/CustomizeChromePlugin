7b4a7c2987ed89e7624164fdb33565fcb058bfcb
Alleyway-boop <3181138299@qq.com>
2024-07-28 22:20:26 +0800

    add SaveData2Server

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,18 +1,21 @@
 interface Message {
-    greeting?: string;
-    async?: boolean;
-    getTabId?: boolean;
-    UpDateLastUseTime?: boolean;
-    getTabActive?: boolean;
-    GetTabStatusList?: boolean;
-    DeleteTab?: boolean;
-    GetFreezeTabList?: boolean;
-    RecoverFreezeTab?: boolean;
-  }
-  interface Response {
-    response: string | any[] | boolean | number | undefined;
-    tabId?: number;
-  }
+  greeting?: string;
+  async?: boolean;
+  getTabId?: boolean;
+  UpDateLastUseTime?: boolean;
+  getTabActive?: boolean;
+  GetTabStatusList?: boolean;
+  DeleteTab?: boolean;
+  GetFreezeTabList?: boolean;
+  RecoverFreezeTab?: boolean;
+}
+interface Response {
+  response: string | any[] | boolean | number | undefined;
+  tabId?: number;
+}
 type SendResponse = (response?: Response) => void;
+// todo 将数据保存到服务器
+function Save2server() {
 
+}
 export type { Message, Response, SendResponse }
