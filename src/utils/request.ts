df54e7cb46643d5fa6f330a46bd6f262dca602ab
Alleyway-boop <3181138299@qq.com>
2024-09-03 22:49:59 +0800

    change alova configuration

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,10 @@
 import { createAlova } from 'alova';
 import fetchAdapter from 'alova/fetch';
-
+import vueHook from 'alova/vue';
 const alovaInstance = createAlova({
+    statesHook: vueHook,
+    cacheFor: null,
+    cacheLogger: true,
     id: `CustomizePlugin:${Date.now()}`,
     baseURL: import.meta.env.baseUrl,
     requestAdapter: fetchAdapter(),
