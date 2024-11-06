ca8c138589fc9bacca2499b1a08e86563c684b79
Alleyway-boop <yuanfangwa@gmail.com>
2024-11-06 20:28:23 +0800

    add input compose input type

--- src/tsxCompose/Y-Input.tsx
+++ src/tsxCompose/Y-Input.tsx
@@ -5,6 +5,12 @@ interface InputProps {
     onChange: (value: string) => void;
     placeholder?: string;
     disabled?: boolean;
+    type?: string;
+    maxlength?: number;
+    onFocus?: () => void;
+    onBlur?: () => void;
+    error?: string;
+    className?: string;
 }
 
 const Input = defineComponent({
@@ -24,6 +30,30 @@ const Input = defineComponent({
         disabled: {
             type: Boolean as PropType<boolean>,
             default: false
+        },
+        type: {
+            type: String as PropType<string>,
+            default: 'text'
+        },
+        maxlength: {
+            type: Number as PropType<number>,
+            default: undefined
+        },
+        onFocus: {
+            type: Function as PropType<() => void>,
+            default: () => {}
+        },
+        onBlur: {
+            type: Function as PropType<() => void>,
+            default: () => {}
+        },
+        error: {
+            type: String as PropType<string>,
+            default: ''
+        },
+        className: {
+            type: String as PropType<string>,
+            default: ''
         }
     },
     setup(props) {
@@ -32,12 +62,20 @@ const Input = defineComponent({
             props.onChange(target.value);
         };
         return () => (
-            <input
-                value={props.value}
-                onInput={handleChange}
-                placeholder={props.placeholder}
-                disabled={props.disabled}
-            />
+            <div>
+                <input
+                    value={props.value}
+                    onInput={handleChange}
+                    placeholder={props.placeholder}
+                    disabled={props.disabled}
+                    type={props.type}
+                    maxlength={props.maxlength}
+                    onFocus={props.onFocus}
+                    onBlur={props.onBlur}
+                    class={props.className}
+                />
+                {props.error && <span class="error-message">{props.error}</span>}
+            </div>
         );
     }
 });
